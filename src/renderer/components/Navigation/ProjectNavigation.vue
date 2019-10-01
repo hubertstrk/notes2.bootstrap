7e28ba1f544a945d97ad514e971f186bce418591
Hubert Stärk <hubert.staerk@gmail.com>
2019-10-01 18:09:32 +0200

    :sparkles: title numbering, project sorting

--- src/renderer/components/Navigation/ProjectNavigation.vue
+++ src/renderer/components/Navigation/ProjectNavigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NavigationCard v-for="project in projects.sort()"
+    <NavigationCard v-for="project in sortedProjects"
       :key="project" :name="project" :selectedName="selectedProject"
       @click="setSelectedProject(project)"
     >
@@ -21,32 +21,35 @@
 </template>
 
 <script>
-  import {mapState, mapMutations, mapGetters} from 'vuex'
+import {mapState, mapMutations, mapGetters} from 'vuex'
 
-  import NavigationCard from './NavigationCard'
-  import NumericTween from '@/components/Shared/NumericTween'
+import NavigationCard from './NavigationCard'
+import NumericTween from '@/components/Shared/NumericTween'
 
-  export default {
-    name: 'Navigation',
-    components: {
-      NavigationCard,
-      NumericTween
-    },
-    computed: {
-      ...mapGetters('editor', [
-        'all',
-        'projects'
-      ]),
-      ...mapState('editor', {
-        selectedProject: state => state.ui.selectedProject
-      })
-    },
-    methods: {
-      ...mapMutations('editor', [
-        'setSelectedProject'
-      ])
+export default {
+  name: 'Navigation',
+  components: {
+    NavigationCard,
+    NumericTween
+  },
+  computed: {
+    ...mapGetters('editor', [
+      'all',
+      'projects'
+    ]),
+    ...mapState('editor', {
+      selectedProject: state => state.ui.selectedProject
+    }),
+    sortedProjects () {
+      return this.projects.sort((p1, p2) => p1.toLowerCase().localeCompare(p2.toLowerCase()))
     }
+  },
+  methods: {
+    ...mapMutations('editor', [
+      'setSelectedProject'
+    ])
   }
+}
 </script>
 
 <style lang="scss" scoped>
