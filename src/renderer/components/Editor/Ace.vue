99a133ce307e2520e36de032ca7c8c6fbdb01538
Hubert Stärk <hstaerk@ixmap.eu>
2019-11-26 23:45:51 +0100

    :bug: resize editor after window size changes

--- src/renderer/components/Editor/Ace.vue
+++ src/renderer/components/Editor/Ace.vue
@@ -1,66 +1,71 @@
 <template>
   <AceEditor v-model="content" :options="options" @init="intialize"
-    lang="markdown" theme="github" />
+    lang="markdown" theme="github" :onresize="resize()" />
 </template>
 
 <script>
-  import { EventBus } from '@/helper/EventBus'
+import { EventBus } from '@/helper/EventBus'
 
-  import AceEditor from 'vue2-ace-editor'
+import AceEditor from 'vue2-ace-editor'
 
-  export default {
-    name: 'Ace',
-    components: {
-      AceEditor
-    },
-    props: {
-      value: String,
-      options: Object
-    },
-    data () {
-      return {
-        editor: null
-      }
-    },
-    computed: {
-      content: {
-        get () { return this.value ? this.value : '' },
-        set (value) { this.$emit('input', value) }
-      }
-    },
-    methods: {
-      intialize: function (editor) {
-        this.editor = editor
-
-        require('brace/mode/markdown')
-        require('brace/snippets/markdown')
-        require('brace/theme/github')
-        require('brace/theme/xcode')
-        require('brace/theme/crimson_editor')
-        require('brace/theme/tomorrow')
-
-        this.editor.setWrapBehavioursEnabled(true)
-        this.editor.setShowFoldWidgets(false)
-        this.editor.setShowPrintMargin(false)
-        this.editor.setHighlightGutterLine(false)
-        this.editor.renderer.setShowGutter(false)
-        this.editor.getSession().setUseWrapMode(true)
-
-        this.editor.setReadOnly(true)
-      }
-    },
-    watch: {
-      value () {
-        this.editor.setReadOnly(!this.value)
-      }
-    },
-    created () {
-      EventBus.$on('editor-insert-template', template => {
-        this.editor.insert(template)
-        this.editor.focus()
-      })
+export default {
+  name: 'Ace',
+  components: {
+    AceEditor
+  },
+  props: {
+    value: String,
+    options: Object
+  },
+  data () {
+    return {
+      editor: null
     }
+  },
+  computed: {
+    content: {
+      get () { return this.value ? this.value : '' },
+      set (value) { this.$emit('input', value) }
+    }
+  },
+  methods: {
+    intialize: function (editor) {
+      this.editor = editor
+
+      require('brace/mode/markdown')
+      require('brace/snippets/markdown')
+      require('brace/theme/github')
+      require('brace/theme/xcode')
+      require('brace/theme/crimson_editor')
+      require('brace/theme/tomorrow')
+
+      this.editor.setWrapBehavioursEnabled(true)
+      this.editor.setShowFoldWidgets(false)
+      this.editor.setShowPrintMargin(false)
+      this.editor.setHighlightGutterLine(false)
+      this.editor.renderer.setShowGutter(false)
+      this.editor.getSession().setUseWrapMode(true)
+      this.editor.resize()
+      this.editor.setReadOnly(true)
+    },
+    resize () {
+      if (this.editor) {
+        this.editor.resize()
+      }
+    }
+  },
+  watch: {
+    value () {
+      this.editor.setReadOnly(!this.value)
+    }
+  },
+  created () {
+    EventBus.$on('editor-insert-template', template => {
+      this.editor.insert(template)
+      this.editor.focus()
+    })
   }
+}
 </script>
 
 <style lang="scss" scoped>
